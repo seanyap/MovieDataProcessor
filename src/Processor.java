bad3c2000215b4a4e6a6e56fc002f1387fd68005
syap0317 <61310256+syap0317@users.noreply.github.com>
2020-04-10 02:19:10 -0400

    Added functionality to parse year

--- src/Processor.java
+++ src/Processor.java
@@ -11,27 +11,27 @@ class Processor {
         MovieData map = new MovieData();
 	Scanner file = new Scanner(new File(fileName));
 	while (file.hasNext()) {             // check if there is next line
-	    String line  = file.nextLine();         // each line is an individual  movie with its info
+	    String line  = file.nextLine();         // each line is an individual movie with its info
+	    if (line.contains("(no genres listed)"))     // check if there is genre
+		continue;                                // no genre so skip this line    
 	    String[] lineContents = line.split(",");        
-   	    String genre = lineContents[lineContents.length-1];    // genre is in the last cell of the array
+   	    String genre = lineContents[lineContents.length-1];    // genre is in the last cell of the array	
 	    String[] genres = null;
+
+	    int yearIndex = line.lastIndexOf('(') + 1;         // add one because year is after parenthesis 
+	    String year = line.substring(yearIndex, yearIndex+4);      // get year
+	    System.out.println(year); 
 	    if (genre.indexOf('|') != -1) {
 	        genres = genre.split("\\|");
 	    }
 	    if (genres != null) {
 	        for (int i=0; i<genres.length; i++) {      // loop through all the genres 
 		    String name = genres[i];
-		    if (name.equals("(no genres listed)"))      // this is not a genre and should not be included in map
-			continue;
-		    else 
-		        map.read(genres[i]);                  // pass each key into map data structure 	     
+	   	    map.read(genres[i]);                  // pass each key into map data structure 	     
 		}
 	    }
 	    else {
-		if (genre.equals("(no genres listed)"))
-		    continue;
-		else   
-		    map.read(genre);
+		map.read(genre);
 	    }
 	}
 	return map;
